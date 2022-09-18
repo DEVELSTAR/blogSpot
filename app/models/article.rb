d9b2077299eedc57c2c328e4081cc4469819dd85
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-09-19 05:22:57 +0530

    added comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
 	validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10, too_short: "must contain atleast %{count} characters", 
                                                maximum: 500 , too_long: "may not exceed %{count} characters"}
+    
+    has_many :comments, dependent: :destroy 
 end
